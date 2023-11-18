fdbdb04180a11adc4db3e7017c02c2a19a4507bd
sekihono <ysekita0325@gmail.com>
2023-11-18 20:32:59 +0900

    商品出品機能　修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -128,7 +128,36 @@ RSpec.describe Item, type: :model do
       #binding.pry
       expect(@item.errors.full_messages).to include("User must exist")
     end
+    it 'カテゴリーが1の場合は登録できない' do
+      @item.category_id = 1
+      @item.valid?
+      #binding.pry
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
 
+    it '商品の状態が1の場合は登録できない' do
+      @item.product_status_id = 1
+      @item.valid?
+      #binding.pry
+      expect(@item.errors.full_messages).to include("Product status can't be blank")
+    end
 
+    it '配送送料の負担が1の場合は登録できない' do
+      @item.shipping_cost_burden_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping cost burden can't be blank")
+    end
+
+    it '発送元の地域が1の場合は登録できない' do
+      @item.origin_region_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Origin region can't be blank")
+    end
+
+    it '発送までの日数が1の場合は登録できない' do
+      @item.delivery_time_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+    end
   end
 end
