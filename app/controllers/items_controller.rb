5ae5170ed930f0b1ffced51c0d36ae185c5ec998
sekihono <ysekita0325@gmail.com>
2023-11-22 21:51:54 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order(created_at: :desc)
-    #binding.pry
   end
   def new
     @item = Item.new
@@ -20,17 +19,34 @@ class ItemsController < ApplicationController
   end
 
   def show
-    #binding.pry
     @item = Item.find(params[:id])
-    #binding.pry
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    elsif unless current_user == @item.user
+      # ユーザーが自身が出品した商品でない場合、トップページにリダイレクト
+          redirect_to root_path
+          end
+    end
   end
 
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit,status: :unprocessable_entity
+    end
+  end  
 
   private
 
   def item_params
     params.require(:item).permit(:product_name, :description, :price, :image, :category_id, :product_status_id, :shipping_cost_burden_id, :origin_region_id, :delivery_time_id).merge(user_id: current_user.id)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 
 end
