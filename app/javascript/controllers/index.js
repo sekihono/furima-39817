28d6fbd327812ee6d6ea4869c94c028fcb271cc6
sekihono <ysekita0325@gmail.com>
2023-12-07 21:56:56 +0900

    挙動確認後の修正１

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,11 @@
-window.addEventListener('load', function(){
+//window.addEventListener('load', function(){
+function sellingprice(){
 
   const priceInput = document.getElementById("item-price")
  // console.log("set")
  // console.log(priceInput)
-  const addTaxPrice = document.getElementById('add-tax-price');
-  const profitElement = document.getElementById('profit');
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profitElement = document.getElementById("profit");
 
   priceInput.addEventListener('input', function() {
  // console.log(priceInput)
@@ -22,17 +23,12 @@ window.addEventListener('load', function(){
     profitElement.textContent = ''; // 価格が無効な場合は表示をクリア
   }
   })
-})
-
-
-// Import and register all your controllers from the importmap under controllers/*
+}
 
 import { application } from "controllers/application"
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+window.addEventListener('DOMContentLoaded', sellingprice)
+//window.addEventListener('load', sellingprice)
